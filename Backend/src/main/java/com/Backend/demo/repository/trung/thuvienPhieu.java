7db8e665cf56fc093fb101cf34aea38a96870a15
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2021-06-06 16:27:27 +0700

    Update thuvienPhieu.java

--- Backend/src/main/java/com/Backend/demo/repository/trung/thuvienPhieu.java
+++ Backend/src/main/java/com/Backend/demo/repository/trung/thuvienPhieu.java
@@ -1,6 +1,5 @@
 package com.Backend.demo.repository.trung;
 
-import org.springframework.data.repository.CrudRepository;
 
 import com.Backend.demo.model.trung.Phieunhapxuat;  
 import org.springframework.data.jpa.repository.JpaRepository;
