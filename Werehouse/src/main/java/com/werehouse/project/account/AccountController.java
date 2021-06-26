4099cef14dc53445e7c721de938d7a788f20d9c6
Quangnkts <Quangnkts1908037@fpt.edu.vn>
2021-06-26 10:10:32 +0700

    security full no jsp

---
+++ Werehouse/src/main/java/com/werehouse/project/account/AccountController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.werehouse.project.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author PC
+ */
+@Controller
+public class AccountController {
+    
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+    @GetMapping("/")
+    public String home(Model model) {
+        return "Home";
+    }
+    
+}
---
+++ Werehouse/src/main/java/com/werehouse/project/service/account/CustomerUserDetailService.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.werehouse.project.service.account;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author PC
+ 
+@Service
+public class CustomerUserDetailService implements UserDetailsService{
+     private EntityManager entityManager;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account;
+        String sql ="SELECT a FROM Account a WHERE a.email = :email";
+        Query query = entityManager.createQuery(sql,String.class);
+        query.setParameter("email",username);
+        List<Account> list = new ArrayList<Account>();
+        list = query.getResultList();
+        if (list.size()>0){
+            account = list.get(0);
+
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+            GrantedAuthority authority = new SimpleGrantedAuthority("ADMIN");
+            grantedAuthorities.add(authority);
+
+            UserDetails userDetails = new User(username,account.getPassword(),grantedAuthorities);
+            return userDetails;
+        } else {
+            new UsernameNotFoundException("Login Fail");
+        }
+        return null;
+    }
+    
+}
+*/
